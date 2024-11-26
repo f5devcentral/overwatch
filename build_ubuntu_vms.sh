c3ea0f72986195f89ebb80e70de90ddc3df260c6
steveh565 <keyhman@gmail.com>
2024-11-26 10:36:57 -0500

    major redesign

--- build_ubuntu_vms.sh
+++ build_ubuntu_vms.sh
@@ -21,6 +21,9 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --
 # k9s
 curl -fsSL https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_linux_amd64.deb -o k9s_linux_amd64.deb && sudo dpkg -i ./k9s_linux_amd64.deb
 
+# filebeat agent
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.15.3-amd64.deb && sudo dpkg -i filebeat-8.15.3-amd64.deb
+
 # Install Docker using get-docker.sh:
 curl -fsSL https://get.docker.com -o get-docker.sh &&
 sudo sh ./get-docker.sh --dry-run &&
