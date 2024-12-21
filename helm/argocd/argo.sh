31207834104598241209c059268db6da10da452b
steveh565 <keyhman@gmail.com>
2024-12-21 16:21:07 -0500

    argocd addon

--- helm/argocd/argo.sh
+++ helm/argocd/argo.sh
@@ -5,22 +5,10 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
 # install ArgoCD-HA
-helm install --upgrade argocd-stack argo-helm/argo-cd -n argocd --create-namespace --values ./values.yaml
+helm install --upgrade argocd-stack argo/argo-cd -n argocd --create-namespace --values ./values.yaml
 
-function installArgoCd () {
-    #ArgoCD Operator Lifecycle Manager
-    curl -L https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.30.0/install.sh -o install.sh
-    chmod +x install.sh
-    ./install.sh v0.30.0
-    kubectl create -n olm -f deploy/catalog_source.yaml
-
-    #ArgoCD Operator
-    kubectl create namespace argocd
-    kubectl create -n argocd -f deploy/operator_group.yaml
-    kubectl create -n argocd -f deploy/subscription.yaml
-    kubectl get installplans -n argocd
-
-    #ArgoCD CRDs
-    kubectl apply -k https://github.com/argoproj/argo-cd/manifests/crds\?ref\=stable
-}
-
+# show elastic-operator logs
+argoPass=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
+echo "Username: elastic"
+echo "Password: ${argoPass}"
+echo ""
